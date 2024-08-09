ba370870936cbb930480aa9614309280559a96f7
dipan samadder <Dipansamadder99@gmail.com>
2024-08-09 11:10:00 +0530

    Render Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import ClickCounter from './components/ClickCounter';
 import HoverCounter from './components/HoverCounter';
+import User from './components/User';
 
 function App() {
   return (
     <div>
       <ClickCounter/>
       <HoverCounter/>
+      <User render={(isLoggedIn)=> isLoggedIn ? 'Dipan Samadder' :'Guset' }/>
     </div>
   )
 }
